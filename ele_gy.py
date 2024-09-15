c083c183f3ad94bf67d7ac54be777a995423c760
October <96674689+qianme@users.noreply.github.com>
2024-09-15 15:15:36 +0800

    Update ele_gy.py

--- ele_gy.py
+++ ele_gy.py
@@ -13,7 +13,7 @@ from datetime import datetime, date
 nczlck = os.environ.get('elmck')
 
 ck = ''
-elm_host = open('xsign_host.txt', 'r', encoding='utf-8').read()
+elm_host = open('xsign_host.txt', 'r', encoding='utf-8').read().split("%")[0]
 
 def tq(txt):
     try:
